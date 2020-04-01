02290913ff6ad1983e03090bbd838f2b0518db11
Dusan <dusan.trtica@hotmail.com>
2020-04-01 18:15:00 +0200

    cache

--- services/cache.js
+++ services/cache.js
@@ -3,17 +3,28 @@ const redis = require('redis');
 const util = require('util');
 const redisUrl = 'redis://localhost:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function(options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
+
+  return this; // to have chainable properties.
+};
+
 mongoose.Query.prototype.exec = async function() {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+
   const key = JSON.stringify({
     ...this.getQuery(),
     collection: this.mongooseCollection.name,
   });
 
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   if (cacheValue) {
     console.log('returned from cache');
@@ -27,7 +38,13 @@ mongoose.Query.prototype.exec = async function() {
   console.log('reading database');
   const result = await exec.apply(this, arguments);
 
-  client.set(key, JSON.stringify(result));
+  client.hmset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
 
   return result;
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  },
 };
