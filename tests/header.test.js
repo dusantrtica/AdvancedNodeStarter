636a4808beebe6df4c159500207eeb68f00dc56a
Dusan <dusan.trtica@hotmail.com>
2020-04-05 19:06:27 +0200

    added login in tests

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const sessionFactory = require('./factories/sessionFactory');
+const userFactory = require('./factories/userFactory');
 
 let browser, page;
 
@@ -23,4 +25,22 @@ test('clicking login starts oauth flow', async () => {
   const url = await page.url();
   console.log({ url });
   expect(url.includes('accounts.google.com'));
+});
+
+test('When signed in, shows logout button', async () => {
+  const id = '5e7c92a184f4a83603bfaad0';
+  const user = await userFactory();
+  const { session, sig } = sessionFactory(user);
+  await page.setCookie({ name: 'session', value: session });
+  await page.setCookie({ name: 'session.sig', value: sig });
+
+  // moramo reload da bi se izmene primenile
+  await page.goto('localhost:3000');
+
+  // moramo da cekamo da se stranica renderuje, ne mozemo samo da rokamo
+  // naredbe
+  await page.waitFor('a[href="/auth/logout"]');
+
+  const text = await page.$eval('a[href="/auth/logout"]', (el) => el.innerHTML);
+  expect(text).toEqual('Logout');
 });
