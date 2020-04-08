8342cda1b49187857c174b341cf31953ef53b420
Dusan <dusan.trtica@hotmail.com>
2020-04-08 19:10:46 +0200

    Page, proxy , factories and helpers

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,17 +1,14 @@
-const puppeteer = require('puppeteer');
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
+const Page = require('./helpers/page');
 
-let browser, page;
+let page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({ headless: false });
-  page = await browser.newPage();
+  page = await Page.build();
   await page.goto('localhost:3000');
 });
 
 afterEach(async () => {
-  await browser.close();
+  await page.close();
 });
 
 test('We can launch a browser', async () => {
@@ -28,19 +25,7 @@ test('clicking login starts oauth flow', async () => {
 });
 
 test('When signed in, shows logout button', async () => {
-  const id = '5e7c92a184f4a83603bfaad0';
-  const user = await userFactory();
-  const { session, sig } = sessionFactory(user);
-  await page.setCookie({ name: 'session', value: session });
-  await page.setCookie({ name: 'session.sig', value: sig });
-
-  // moramo reload da bi se izmene primenile
-  await page.goto('localhost:3000');
-
-  // moramo da cekamo da se stranica renderuje, ne mozemo samo da rokamo
-  // naredbe
-  await page.waitFor('a[href="/auth/logout"]');
-
+  await page.login();
   const text = await page.$eval('a[href="/auth/logout"]', (el) => el.innerHTML);
   expect(text).toEqual('Logout');
 });
